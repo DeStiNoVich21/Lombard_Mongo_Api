c562246ad822aeb6ea7e8126fe96b3449abe8619
ta1hou <118274604+ta1hou@users.noreply.github.com>
2024-03-05 13:00:28 +0600

    теперь есть две бд локал и удаленка на локалке тестим у себя на удаленной уже будет проект работать

---
+++ Lombard_Mongo_Api/Controllers/LombardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lombard_Mongo_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LombardController : ControllerBase
+    {
+    }
+}
--- Lombard_Mongo_Api/Models/Lombards.cs
+++ Lombard_Mongo_Api/Models/Lombards.cs
@@ -5,30 +5,20 @@ using System.Text.Json.Serialization;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using Microsoft.AspNetCore.Components.Web;
-
 namespace Lombard_Mongo_Api.Models
 {
     public class Lombards
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-
-        public string _id { get; set; }
+        public string _id { get; set; } = BsonObjectId.GenerateNewId().ToString();
         [BsonElement("name")]
         public string lombard_name { get; set; } = "default_name";
-
         [BsonElement("address")]
         public string? address { get; set; } = string.Empty;
-
         [BsonElement("number")]
         public string? number { get; set; } = string.Empty;
-
         [BsonElement("description")]
         public string? description { get; set; } = string.Empty;
-        
-
-
-       
-
     }
 }
