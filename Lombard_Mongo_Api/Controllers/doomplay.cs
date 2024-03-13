e6c530898acc235d4c0bc2c9a06be17c06431fc6
ta1hou <118274604+ta1hou@users.noreply.github.com>
2024-03-13 17:22:42 +0500

    добавлена посхалка

---
+++ Lombard_Mongo_Api/Controllers/doomplay.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lombard_Mongo_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class doomplay : ControllerBase
+    {
+        private const string IDDQD = "IDDQD";
+
+        [HttpGet]
+        public IActionResult Get(string cheatCode)
+        {
+            if (string.Equals(cheatCode, IDDQD, System.StringComparison.OrdinalIgnoreCase))
+            {
+                // Если передан верный чит-код, возвращаем ссылку
+                string gameLink = "https://dos.zone/doom-dec-1993/";
+                return Redirect(gameLink);
+            }
+            else
+            {
+                // Возвращаем сообщение об ошибке, если чит-код неверный
+                return BadRequest("Invalid cheat code");
+            }
+        }
+    }
+}
