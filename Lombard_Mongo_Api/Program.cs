18362ecadbed997792f324ea065b838bb6816675
ICEY_YOU <122779819+DeStiNoVich21@users.noreply.github.com>
2025-04-02 23:36:51 +0500

    git fix

--- Lombard_Mongo_Api/Program.cs
+++ Lombard_Mongo_Api/Program.cs
@@ -94,12 +94,13 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
